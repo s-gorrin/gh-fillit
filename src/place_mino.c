d0def2a03d21ba6bf28dedec46a4d51e218b2920
Seth Gorrin <sgorrin@gmail.com>
2018-04-19 20:37:46 -0700

    Getting less dumb by the minute

--- src/place_mino.c
+++ src/place_mino.c
@@ -6,7 +6,7 @@
 /*   By: sgorrin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 19:27:51 by sgorrin           #+#    #+#             */
-/*   Updated: 2018/04/19 20:15:53 by sgorrin          ###   ########.fr       */
+/*   Updated: 2018/04/19 20:37:27 by sgorrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	place_mino(t_mino *mino, t_map *map, int i)
 
 	ni = 0;
 	counter = 0;
-	if (ft_strchr(map->mapstr, mino->letter))
+	if (mino->location >= 0)
 		return (0);
 	while ((mino->minostr)[ni])
 	{
