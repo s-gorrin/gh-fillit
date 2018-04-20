eaf0c1e9a68bca5ec91934534aa50c5f0f6e787b
Seth Gorrin <sgorrin@gmail.com>
2018-04-19 21:17:24 -0700

    Place mino is getting a lot of attention

--- src/place_mino.c
+++ src/place_mino.c
@@ -6,16 +6,31 @@
 /*   By: sgorrin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 19:27:51 by sgorrin           #+#    #+#             */
-/*   Updated: 2018/04/19 20:45:10 by sgorrin          ###   ########.fr       */
+/*   Updated: 2018/04/19 21:13:48 by sgorrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	place_mino(t_mino *mino, t_map *map, int i)
+static void	placer(t_mino *mino, t_map *map, int i)
 {
-	int		n;
-	int		ni;
+	int	n;
+	int	ni;
+
+	ni = 0;
+	while ((mino->minostr)[ni])
+	{
+		n = mod_num(map->mapsize, to_int(mino->minostr[ni]));
+		(map->mapstr)[i + n] = mino->letter;
+		ni++;
+	}
+}
+
+int			place_mino(t_mino *mino, t_map *map, int i)
+{
+	int	n;
+	int	ni;
+	int	counter;
 
 	ni = 0;
 	counter = 0;
@@ -25,17 +40,16 @@ int	place_mino(t_mino *mino, t_map *map, int i)
 	{
 		n = mod_num(map->mapsize, to_int(mino->minostr[ni]));
 		if ((i + n) < MAP_AREA && (map->mapstr)[i + n] == '.')
-		{
-			(map->mapstr)[i + n] = mino->letter;
 			counter++;
-		}
 		ni++;
 	}
 	if (counter = 4)
 	{
+		placer(mino, map, i);
 		mino->location = i;
 		return (1);
 	}
 	else
 		return (0);
 }
+
