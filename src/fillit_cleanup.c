9518712c7a38c88a9a7f3f2c1430e418a0381fa2
Scott Snelgrove <ssnelgro@e1z2r2p1.42.us.org>
2018-04-24 14:49:22 -0700

    Worked on fillit_cleanup

--- src/fillit_cleanup.c
+++ src/fillit_cleanup.c
@@ -6,7 +6,7 @@
 /*   By: sgorrin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 17:03:00 by sgorrin           #+#    #+#             */
-/*   Updated: 2018/04/23 17:09:43 by snake            ###   ########.fr       */
+/*   Updated: 2018/04/24 14:48:26 by ssnelgro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 void	fillit_cleanup(t_map *map, t_mino **mino)
 {
-	free(mino);
+	t_mino *current;
+	t_mino *tmp;
+	int count;
+
+	count = map->num_mino;
+	current = *mino;
+	tmp = NULL;
+	while (count)
+	{
+		tmp = current;
+		current++;
+		free(tmp);
+		count--;
+	}
+
+	free(map->mapstr);
 	free(map);
 }
